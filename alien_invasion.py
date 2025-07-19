164f9cd153fe8a7f8314006ce4824a33b61ccc30
LionWarrior2008 <nrajeev@student.cscc.edu>
2025-07-19 17:27:30 -0400

    Add files via upload

---
+++ alien_invasion.py
@@ -0,0 +1,77 @@
+import sys
+import pygame
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+
+
+class AlienInvasion:
+
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+        self.screen=pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_width=self.screen.get_rect().width
+        self.settings.screen_height=self.screen.get_rect().height
+
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self.bullets.update()
+            self._update_screen()
+            self.clock.tick(60)
+
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type ==pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type ==pygame.KEYUP:
+                self._check_keyup_events(event)
+    def _check_keydown_events(self,event):
+        if event.key==pygame.K_s:
+                self.ship.moving_down=True
+        elif event.key==pygame.K_w:
+                self.ship.moving_up=True
+        elif event.key == pygame.K_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullets() 
+    def _check_keyup_events(self,event):
+        if event.key==pygame.K_s:
+            self.ship.moving_down=False
+        elif event.key==pygame.K_w:
+            self.ship.moving_up=False
+    def _fire_bullets(self):
+        new_bullet=Bullet(self)
+        self.bullets.add(new_bullet)
+
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new screen."""
+        self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        self.ship.blitme()
+        self.ship
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
+
+if __name__ == '__main__':
+    pass
